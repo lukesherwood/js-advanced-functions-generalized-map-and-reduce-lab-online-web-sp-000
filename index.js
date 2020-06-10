c762f17fe728e5967dc059249ee20c7ceea05763
Luke Sherwood <luke.sherwood55@gmail.com>
2020-06-10 23:06:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(array, cb) {
+  let r = []
+  for (const element of array) {
+    r.push(cb(element))
+  }
+  return r;
+}
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+  return r;
+}
